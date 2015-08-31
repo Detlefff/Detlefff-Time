3ca6d6c50b9848150da9c8dcefc6e74cacd2c49f
Jannick Fahlbusch <git@jf-projects.de>
2015-08-31 10:51:58 +0200

    Change the accesslevel and use DATE_RFC822

--- time.php
+++ time.php
@@ -7,10 +7,10 @@ date_default_timezone_set('UTC');
  */
 class time extends Script
 {
-    private $helpMessage = 'Returns the actual UTC time';
+    protected $helpMessage = 'Returns the actual UTC time';
 
     public function run()
     {
-        return $this->send($this->message->number, date(RFC822));
+        return $this->send($this->message->number, date(DATE_RFC822));
     }
 }
