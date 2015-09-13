109030e6819850be4818f29227a1870b447644f7
Jannick Fahlbusch <git@jf-projects.de>
2015-09-13 22:44:28 +0200

    Complete the information for the help-plugin

--- time.php
+++ time.php
@@ -1,11 +1,10 @@
 <?php
 date_default_timezone_set('UTC');
-/**
- *
- */
+
 class time extends Script
 {
-    protected $helpMessage = 'Returns the actual UTC time';
+    protected $helpMessage = "'time'\n";
+    protected $description = 'Returns the actual UTC-time';
 
     public function run()
     {
